88b626040251674effc9dfd664e2c62befe38e2a
IhorAlexsovich <alexsovich1994@gmail.com>
2022-09-02 14:18:37 +0300

    finish

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,27 @@
+import { Button, ButtonStyle } from './FeedbackOptions.styled';
+import PropTypes from 'prop-types';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ButtonStyle>
+      {options.map(option => {
+        // console.log(option);
+        return (
+          <Button
+            type="button"
+            key={option}
+            id={option}
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </Button>
+        );
+      })}
+    </ButtonStyle>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
+};
